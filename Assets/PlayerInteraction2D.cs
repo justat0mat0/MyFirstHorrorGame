f8e5168c2864d68af9247cc59893f229158cc0aa
justat0mat0 <1278813924@qq.com>
2026-04-21 22:44:51 +0800

    0421-场地素材替换/场地移动关联/对话框

---
+++ Assets/PlayerInteraction2D.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction2D : MonoBehaviour
+{
+    public IInteractable2D overlappingInteractable;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (overlappingInteractable != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                overlappingInteractable.Interact();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IInteractable2D interactable = collision.gameObject.GetComponent<IInteractable2D>();
+        if (interactable != null)
+        {
+            interactable.tipsIcon.SetActive(true);
+            overlappingInteractable = interactable;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IInteractable2D interactable = collision.GetComponent<IInteractable2D>();
+        if (interactable != null && interactable == overlappingInteractable)
+        {
+            interactable.tipsIcon.SetActive(false);
+            overlappingInteractable = null;
+        }
+    }
+}
